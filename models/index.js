1349f6b1d0b9c0302931eb5eb6d2d43ba8c981d9
hmalle <hmalle90ak@gmail.com>
2018-07-17 10:38:59 -0700

    n=n+1

--- models/index.js
+++ models/index.js
@@ -6,7 +6,12 @@ const env       = process.env.NODE_ENV || 'development';
 const config    = require(__dirname + '/../config/config.json')[env];
 const db        = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+var sequelize = 0;
+if(config.use_env_variable){
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
+}else{
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs.readdirSync(__dirname).filter(function(file) {
   return (file.indexOf('.') !== 0) && (file !== "index.js");
