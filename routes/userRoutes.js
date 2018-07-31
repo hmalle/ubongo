3b7a3261addfb83f9fc5c1c5802fb2a8dfaa8287
hmalle <hmalle90ak@gmail.com>
2018-07-31 09:40:15 -0700

    heroku

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 module.exports = (app) => {
   app.get("/", (req,res)=>{
-    res.sendFile(path.join(__dirname+"./client/build/index.html"));
+    res.sendFile(path.join(__dirname+"../client/public/index.html"));
   });
 
   app.post("/api/signin", passport.authenticate("local"), (req,res)=>{
