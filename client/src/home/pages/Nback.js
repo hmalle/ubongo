bc7ca1494c740806549d9e70f29dd89c2a9858f4
hmalle <hmalle90ak@gmail.com>
2018-11-27 16:32:31 -0800

    Added an implementation of NBack, still in dev

--- client/src/home/pages/Nback.js
+++ client/src/home/pages/Nback.js
@@ -1,31 +1,91 @@
 
 import React, { Component } from "react";
-import css from "./pagestyles.js";
+import navcss from "./pagestyles.js";
 //import axios from "axios";
 
 export default class puzzles  extends Component{
   state={
-
+    data: [],
+    colors: [],
+    symbols:"1234567890",
+    position:"1234567890",
+    sounds:"qwertyuiopasdfghjklzxcvbnm",
+    initiated: false,
   }
   
   generateNewSession(){
+    let tmpdata = [];
+    for(let a=0; a<9; a++){
+      tmpdata.push({
+        sound: this.state.sounds.split("")[Math.floor(Math.random()*26)],
+        symbol: this.state.symbols.split("")[Math.floor(Math.random()*10)],
+        position: this.state.position.split("")[Math.floor(Math.random()*10)],
+        color: "blue",
+      });
+    }
+    this.setState({
+      data: tmpdata,
+      initiated: true
+    });
   }
 
   render(){
+    var content
+    if(this.state.initiated){
+      console.log("+++ The data is " + JSON.stringify(this.state.data));
+      content = (
+        <div>
+          {this.state.data.map((eachData, index) => (
+            <div style={css.grid} 
+              key={index}
+              className="col-md-4">
+              <h1 style={css.symbols}>{eachData.symbol}</h1>
+            </div>
+          ))}
+        </div>
+      );
+    }else {
+      //Nothing 
+      content = <div></div>
+    }
+
     return(
       <div>
         <div>
           <ul className="nav">
             <li className="nav-item">
-              <a style={css.header}>N-Back</a>
+              <a style={navcss.header} onClick={()=>{this.generateNewSession()}}>Start</a>
             </li>
           </ul>
         </div>
         <div>
-          <h2>Nothing here at the moment</h2>
+          <div className="row">
+            <div className="col-md-3"></div>
+            <div className="col-md-6" style={css.nbackbox}>
+              {content}
+            </div>
+            <div className="col-md-3"></div>
+          </div>
         </div>
       </div>
     )
   };
 }
 
+const css={
+  header:{
+
+  },
+  nbackbox:{
+  },
+  grid:{
+    border: "2px solid black",
+    marginTop: 3,
+    borderRadius: 3,
+    height: 200,
+  },
+  symbols:{
+    fontSize: 200,
+    color: "blue",
+  },
+}
